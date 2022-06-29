aa2ea4f0c8dbebada6653eb9a857db9b2f351143
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2022-06-29 19:58:19 +0300

    3

--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,26 @@
 import { renderBlock } from './lib.js'
 
-export function renderUserBlock (favoriteItemsAmount) {
+// class User {
+//   userName: string
+//   userAvatar: HTMLImageElement
+//   favAmount: number
+//
+//   constructor(userName: string, userAvatar: HTMLImageElement, favAmount: number) {
+//     this.userName = userName
+//     this.userAvatar = userAvatar
+//     this.favAmount = favAmount
+//   }
+// }
+
+// let userName = document.getElementById('user')
+// let userAvatar = document.getElementById('avatar')
+// let favAmount = document.getElementById('favAmount')
+
+export function renderUserBlock (
+  favoriteItemsAmount: number,
+  userName: string,
+  userAvatar: HTMLImageElement
+) {
   const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'
   const hasFavoriteItems = favoriteItemsAmount ? true : false
 
@@ -8,9 +28,9 @@ export function renderUserBlock (favoriteItemsAmount) {
     'user-block',
     `
     <div class="header-container">
-      <img class="avatar" src="/img/avatar.png" alt="Wade Warren" />
+      <img class="avatar" id="avatar" src="/img/avatar.png" alt="Wade Warren" />
       <div class="info">
-          <p class="name">Wade Warren</p>
+          <p class="name" id="user">Wade Warren</p>
           <p class="fav">
             <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
           </p>
